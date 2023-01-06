df12ad3ac6669376baba0dd958d45a63dea4cf97
dluman <dluman@allegheny.edu>
2023-01-05 23:02:23 -0500

    Updating with nightly progress

--- index.js
+++ index.js
@@ -4,12 +4,33 @@ const core = require('@actions/core');
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
+const yaml = require('js-yaml');
+
+const mustache = require('mustache');
 
 //const octokit = github.getOctokit(
 //    process.env.GITHUB_TOKEN
 //);
 
-const readReport = (filename) => util.promisify(fs.readFile)(filename, 'utf8');
+const loadFile = (filename) => util.promisify(fs.readFile)(filename, 'utf8');
+
+async function postIssue(checks) {
+  loadTemplate();
+}
+
+const loadTemplate = async () => {
+  let template = await loadFile("templates/IssueTemplate.md");
+}
+
+// TODO: Definte recursive read to descend into each descriptor
+//       and find the gold?
+
+const getCategories = async (checks) => {
+  let definitions = await loadFile(".gatorgrade.yml");
+  let data = yaml.load(definitions);
+  // TODO: Load relevant categories from the YAML file based on
+  //       in-text check names
+}
 
 const cleanLines = (lines) => {
   // Remove blanks
@@ -22,7 +43,7 @@ const cleanLines = (lines) => {
 
 const getChecks = (lines) => {
   // Separate checks from irrelevant lines
-  let checkSymbols = ["✔","✘","➔","→"];
+  let checkSymbols = ["✔","✘"]//,"➔","→"];
   let regexp = new RegExp(`(${checkSymbols.join("|")})`,"g");
   lines = lines.filter(line => !line.search(regexp))
   // Sort checks into object
@@ -31,19 +52,28 @@ const getChecks = (lines) => {
     "failed": []
   }
   for(let check of lines) {
+    // Get success or failure
     let status = check[0];
-    if(status == "✔") checks.passed.push(check);
-    else checks.failed.push(check);
+    // Retrieve the body of the check
+    let body = check.substring(1).trim();
+    if(status == "✔") checks.passed.push(body);
+    // TODO: Really, combine failures into a "check" and annotation
+    //       (see above regex commented out)
+    else checks.failed.push(body);
   }
   return checks;
 }
 
 const run = async () => {
-  let report = await readReport("report");
+  // Acquire checks from cached file
+  let report = await loadFile("report");
   let lines = cleanLines(
       report.split("\n")
   );
-  console.log(getChecks(lines));
+  let checks = getChecks(lines);
+  checks = await getCategories(checks);
+  // Post issue
+  postIssue(checks);
 };
 
 run();
---
+++ templates/IssueTemplate.md
@@ -0,0 +1,16 @@
+# Failures
+
+Looks like we've still got some stuff to work on.
+
+{{#items}}
+{{#label}}
+## {{label}}
+{{#issues}}
+- [ ] {{.}}
+{{/issues}}
+{{/label}}
+{{/items}}
+
+## Percent complete
+
+Based on the checks achieved, this assignment is {{pct}}% complete.
