949637a172273ce17877a48519b314bd744451c6
dluman <dluman@allegheny.edu>
2023-01-06 14:08:11 -0500

    Updating test tag

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ const getChecks = (result, grader) => {
     checks.push(check);
   }
   Object.values(checks).some((check) => {
+    console.log(check);
     if(result.passed.includes(check.description))
       check.status = true;
   });
@@ -93,7 +94,7 @@ const getResult = (lines) => {
     let status = check[0];
     // Retrieve the body of the check
     let body = check.substring(1).trim();
-    console.log(status);
+    console.log(`${status} ${body}`);
     if(status == "✔" || status == "✓") checks.passed.push(body);
     // TODO: Really, combine failures into a "check" and annotation
     //       (see above regex commented out)
